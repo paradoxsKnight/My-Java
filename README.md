6f1fcec48b3fc8136dcc6995b27642a5dcb95a07
Sugata Dutta <duttasugata28@gmail.com>
2025-02-10 14:07:15 +0530

    my first java program to git

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # My-Java
+This is my first java program uploding to git
---
+++ p1.java
@@ -0,0 +1,13 @@
+// Exception hendaling
+class p1
+{
+    public static void main(String []args)
+    {  
+      
+       int a[]={1,2,3,45};
+       for( int i : a)
+       {
+         System.out.println(i);
+       }
+    }
+}
